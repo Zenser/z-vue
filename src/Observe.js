6094223ba9c922b8db59e2779842a98243c40166
张帅 <zhangshuai@cashbus.com>
2018-03-10 17:26:38 +0800

    comment

--- src/Observe.js
+++ src/Observe.js
@@ -6,17 +6,21 @@ export default class Observe {
 }
 
 function reactive(obj, prop, val) {
+    // 闭包绑定依赖
     let dep = new Dep()
     Object.defineProperty(obj, prop, {
         configurable: true,
         enumerable: true,
         get() {
+            //利用js单线程，在初次get时绑定订阅者
             if (Dep.target) {
+                // 绑定订阅者
                 dep.addSub(Dep.target)
             }
             return val
         },
         set(newVal) {
+            // 更新时，触发订阅者更新
             dep.notify(newVal, val)
             val = newVal
         }
